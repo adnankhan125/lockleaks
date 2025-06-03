591c1c26f19573943ad8bda230e9840be44679e8
mamirhashmi <mamirhashmi5@gmail.com>
2025-06-03 23:41:05 +0500

    changes4

--- pages/agencies.tsx
+++ pages/agencies.tsx
@@ -3,15 +3,16 @@ import '../styles/footer.css';
 import '../styles/global.css';
 import '../styles/termsConditions.css';
 import '../styles/AgenciesVideo.css';
-import '../styles/faqs.css';
+// import '../styles/faqs.css';
 
 import Header from '@/components/header';
 import AgenciesSection from '@/components/AgenciesSection';
 import AgenciesVideo from '@/components/AgenciesVideo';
 import HowItWorks from '@/components/HowItWorks';
-import Faqs from '@/components/faqs';
+// import Faqs from '@/components/faqs';
 import Footer from '@/components/footer';
 import SimpleComparison from '@/components/SimpleComparison';
+import TestiScrol from '@/components/TestiScrol';
 
 
 export default function Contact() {
@@ -22,7 +23,8 @@ export default function Contact() {
       <AgenciesVideo/>
       <HowItWorks/>
       <SimpleComparison/>
-      <Faqs />
+      <TestiScrol/>
+      {/* <Faqs /> */}
       <Footer />
       
     </>
