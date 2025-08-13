1f3c0e2817eab063e016856bba2a32c312574ef1
mamirhashmi <mamirhashmi5@gmail.com>
2025-08-13 19:07:57 +0500

    12aug

--- pages/services.tsx
+++ pages/services.tsx
@@ -72,7 +72,8 @@ export default function ServicesPage() {
           </div>
       
 
-      <ProtectPopup />
+      {/* <ProtectPopup /> */}
+
       <FaqSection faqs={serviceFaqs} heading="Frequently Asked Questions" />
       <CallToAction />
       <Footer />
