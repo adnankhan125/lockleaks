ea3fbaec6f927688245224d12aa209781ad0750b
mamirhashmi <mamirhashmi5@gmail.com>
2025-09-30 00:13:20 +0500

    29sep

--- components/Method.tsx
+++ components/Method.tsx
@@ -5,20 +5,20 @@ const Method: React.FC = () => {
   return (
     <div className="method-container">
       <div className="method-section">
-        <h3>Method</h3>
+        {/* <h3>Method</h3> */}
         
         <div className="row first-row">
           <div className="col-md-6">
             <div className="form-row">
-              <label htmlFor="background">Background Method</label>
-              <input type="text" id="background" className="form-control" placeholder="Enter background URL" />
+              <label htmlFor="background">Method</label>
+              <input type="text" id="background" className="form-control cstm-frm" placeholder="Enter background URL" />
               <button className="save-btn mt-2">Save</button>
             </div>
           </div>
           <div className="col-md-6">
             <div className="form-row">
               <label htmlFor="contact">Contact(s)</label>
-              <input type="text" id="contact" className="form-control" placeholder="Enter contact URL" />
+              <input type="text" id="contact" className="form-control cstm-frm" placeholder="Enter contact URL" />
               <button className="save-btn mt-2">Save</button>
             </div>
           </div>
@@ -28,7 +28,7 @@ const Method: React.FC = () => {
           <div className="col-md-12">
             <div className="form-row">
               <label htmlFor="test">Test Link(s)</label>
-              <input type="text" id="test" className="form-control" placeholder="Enter test link" />
+              <input type="text" id="test" className="form-control cstm-frm" placeholder="Enter test link" />
               <button className="save-btn mt-2">Save</button>
             </div>
           </div>
--- components/WebsiteCards.tsx
+++ components/WebsiteCards.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { FaGlobe } from 'react-icons/fa'; // Import the globe icon from react-icons
 import '../styles/WebsiteCards.css'; // Import the CSS file for styling
+import '../styles/ReportALeak.css';
 
 const WebsiteCards: React.FC = () => {
   return (
     <div className="container py-4 custom-container">
       <div className="row">
         {/* Search Field Section */}
+                  <div className= "heading-filter">Search</div>
+
         <div className="col-md-12 mb-4">
           <div className="search-container">
+            
             <input
               type="text"
               placeholder="Search or enter a domain"
@@ -19,7 +23,7 @@ const WebsiteCards: React.FC = () => {
         </div>
 
         <div className="col-md-4">
-          <div className= "heading-filter"> <h6>Filters</h6></div>
+          <div className= "heading-filter">Filters</div>
           <button className="btn btn-primary-ok-black">Removable</button>
           <button className="btn btn-primary-ok-black">Monitoring</button><br />
           <button className="btn btn-primary-ok-black">seo (6185)</button>
@@ -55,13 +59,23 @@ const WebsiteCards: React.FC = () => {
                   <span className="tag traffic">10,224,920 traffic</span>
                   <span className="tag removable">Removable</span>
                 </div>
-
               </div>
             </div>
           ))}
         </div>
       </div>
+        <div className="d-flex justify-content-center">
+              <nav>
+                <ul className="pagination pagination-sm">
+                  <li className="page-item"><a className="page-link" href="#">1</a></li>
+                  <li className="page-item"><a className="page-link" href="#">2</a></li>
+                  <li className="page-item"><a className="page-link" href="#">10</a></li>
+                  <li className="page-item"><a className="page-link" href="#">15</a></li>
+                </ul>
+              </nav>
+            </div>
     </div>
+    
   );
 };
 
